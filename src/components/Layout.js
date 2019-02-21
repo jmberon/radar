e7edac7d64d27fc1af63f441db3b3e9a424ae15b
JABBA\msi <jb@appstab.co>
2019-02-21 22:49:57 +0800

    New hero images. Services Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -18,16 +18,38 @@ export default ({ children }) => (
                 </div>
             </div>
 
-            <div class="main">
+            <div className="main">
                 <section className="figures">
                     <h5>
                         Puedes aumentar tu dinero disponible en caja, monitoreando tu inventario en tiempo real SIN PARAR TU NEGOCIO
                     </h5>
                     <ul>
-                        <li>A</li>
-                        <li>B</li>
-                        <li>C</li>
+                        <li>
+                            <div className="card">
+                                <b className="digits">11</b>
+                                <span>AÑOS</span>
+                                <span>de investigación</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="card">
+                                <b className="digits">150</b>
+                                <span>Negocios de motos y</span>
+                                <span>autopartes estudiados</span>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="card">
+                                <b className="digits">3</b>
+                                <span>Dimensiones estudiadas</span>
+                                <span>Procesos/Personas/</span>
+                                <span>Infraestructura</span>
+                            </div>
+                        </li>
                     </ul>
+                    <p>
+                        ¡Desarrollamos un modelo PRÁCTICO, FÁCIL y RÁPIDO para gestionar compras e inventarios que realmente te sorprenderá!
+                    </p>
                 </section>
                 <section className="services">
 
