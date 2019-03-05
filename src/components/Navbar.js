7ea43b2e951aeadef501345161483866567a257d
JABBA\msi <jb@appstab.co>
2019-03-05 21:09:21 +0800

    Comment out.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,9 +19,9 @@ const Navbar = () => (
                 <ListLink to="/">Inicio</ListLink>
                 <ListLink to="/tips/">Tips</ListLink>
                 <ListLink to="/news/">Noticias</ListLink>
-                <a className="burger" onclick="myFunction()">
+                {/* <a className="burger" onclick="myFunction()">
                     <i className="fa fa-bars">°°°</i>
-                </a>
+                </a> */}
             </ul>
         </nav>
     </header>
