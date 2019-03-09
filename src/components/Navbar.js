4638830fd2abe14ecf612b0a134df4d89431f62f
JABBA\msi <jb@appstab.co>
2019-03-09 20:49:45 +0800

    navbar responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "gatsby";
 import logo from "../images/logo.png";
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -14,22 +14,32 @@ const ListLink = props => (
 )
 
 
-const Navbar = () => (
-    <header>
-        <Link className="logo" to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-            <img src={logo} alt="Radar, Control en Movimiento"></img>
-        </Link>
-        <nav>
-            <ul>
+function Navbar() {
+
+    const [isCollapsed, toggleNav] = useState(true);
+
+    return (
+        <header>
+            <Link className="logo" to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+                <img src={logo} alt="Radar, Control en Movimiento"></img>
+            </Link>
+            <nav>
+                <ul>
+                    <ListLink to="/">Inicio</ListLink>
+                    <ListLink to="/tips/">Tips</ListLink>
+                    <ListLink to="/news/">Noticias</ListLink>
+                    <span>
+                        <FontAwesomeIcon icon="bars" size="lg" color="#5D5D5D" onClick={() => toggleNav(!isCollapsed)}></FontAwesomeIcon>
+                    </span>
+                </ul>
+            </nav>
+            {!isCollapsed && <ul className="responsiveNav">
                 <ListLink to="/">Inicio</ListLink>
                 <ListLink to="/tips/">Tips</ListLink>
                 <ListLink to="/news/">Noticias</ListLink>
-                <span>
-                    <FontAwesomeIcon icon="bars" size="lg" color="#5D5D5D" onclick="myFunction()"></FontAwesomeIcon>
-                </span>
-            </ul>
-        </nav>
-    </header>
-)
+            </ul>}
+        </header>
+    );
+}
 
 export { Navbar }
