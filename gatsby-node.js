bdcc87926d7f29eb2058a3370b1e602c3ca7be03
JABBA\msi <jb@appstab.co>
2019-05-01 22:34:28 +0800

    Remove pages graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,21 +4,6 @@ const path = require(`path`);
 const slash = require(`slash`);
 const createPaginatedPages = require('gatsby-paginate');
 
-const pageQuery = `
-{
-  allWordpressPage {
-    edges {
-      node {
-        id
-        slug
-        status
-        template
-      }
-    }
-  }
-}
-`
-
 const postsQuery = `
 {
   allWordpressPost {
@@ -48,71 +33,44 @@ const postsQuery = `
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
 
-
   return new Promise((resolve, reject) => {
 
-    // Pages
-    graphql(pageQuery)
+    graphql(postsQuery)
       .then(result => {
         if (result.errors) {
-          console.log(result.errors);
           reject(result.errors);
         }
+        const postTemplate = path.resolve("./src/templates/post.js");
+        const postsTemplate = path.resolve("./src/templates/posts.js");
 
-        const pageTemplate = path.resolve("./src/templates/page.js");
+        // Create Posts
+        createPage({
+          path: `/posts/`,
+          component: slash(postsTemplate)
+        });
 
-        _.each(result.data.allWordpressPage.edges, edge => {
+        // Paginate Posts
+        createPaginatedPages({
+          edges: result.data.allWordpressPost.edges,
+          createPage: createPage,
+          pageTemplate: 'src/templates/posts.js',
+          pageLength: 6,
+          pathPrefix: 'posts',
+          buildPath: (index, pathPrefix) =>
+            index > 1 ? `${pathPrefix}/${index}` : `/${pathPrefix}`, // This is optional and this is the default
+        })
+
+        _.each(result.data.allWordpressPost.edges, edge => {
           createPage({
-            path: `/${edge.node.slug}/`,
-            component: slash(pageTemplate),
+            path: `/post/${edge.node.slug}/`,
+            component: slash(postTemplate),
             context: {
               id: edge.node.id,
             },
           });
         });
-      })
-
-      .then(() => {
-        graphql(postsQuery)
-          .then(result => {
-            if (result.errors) {
-              console.log(result.errors);
-              reject(result.errors);
-            }
-            const postTemplate = path.resolve("./src/templates/post.js");
-            const postsTemplate = path.resolve("./src/templates/posts.js");
-
-            // Create Posts
-            createPage({
-              path: `/posts/`,
-              component: slash(postsTemplate)
-            });
-
-            console.log("allPosts", result.data.allWordpressPost.edges);
-
-            // Paginate Posts
-            createPaginatedPages({
-              edges: result.data.allWordpressPost.edges,
-              createPage: createPage,
-              pageTemplate: 'src/templates/posts.js',
-              pageLength: 6,
-              pathPrefix: 'posts',
-              buildPath: (index, pathPrefix) =>
-                index > 1 ? `${pathPrefix}/${index}` : `/${pathPrefix}`, // This is optional and this is the default
-            })
-
-            _.each(result.data.allWordpressPost.edges, edge => {
-              createPage({
-                path: `/post/${edge.node.slug}/`,
-                component: slash(postTemplate),
-                context: {
-                  id: edge.node.id,
-                },
-              });
-            });
-            resolve();
-          });
+        resolve();
       });
-    // ==== END POSTS ====
   });
-};
+  // ==== END POSTS ====
+}
